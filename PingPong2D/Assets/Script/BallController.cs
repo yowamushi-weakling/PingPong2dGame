457c76eb1c560d2d18d4b901ecb020c6193d99d0
yowamushi-weakling <73294938+yowamushi-weakling@users.noreply.github.com>
2020-11-15 13:16:50 +0700

    Making Gameplay
    Including:
    - Making Background
    - Making Player
    - Making Ball
    - Making Border

---
+++ PingPong2D/Assets/Script/BallController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public int force;
+
+    Rigidbody2D rigid;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        Vector2 direction = new Vector2(2, 0).normalized;
+        rigid.AddForce(direction * force);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void ResetBall()
+    {
+        transform.localPosition = new Vector2(0, 0);
+        rigid.velocity = new Vector2(0, 0);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name == "TepiKanan")
+        {
+            ResetBall();
+            Vector2 direction = new Vector2(-2, 0).normalized;
+            rigid.AddForce(direction * force);
+        }
+        if (collision.gameObject.name == "TepiKiri")
+        {
+            ResetBall();
+            Vector2 direction = new Vector2(2, 0).normalized;
+            rigid.AddForce(direction * force);
+        }
+        if(collision.gameObject.name == "Paddle1" || collision.gameObject.name == "Paddle2")
+        {
+            float angel = (transform.position.y - collision.transform.position.y) * 5f;
+            Vector2 direction = new Vector2(rigid.velocity.x, angel).normalized;
+            rigid.velocity = new Vector2(0, 0);
+            rigid.AddForce(direction * force * 2);
+        }
+    }
+}
---
+++ PingPong2D/Assets/Script/PaddleController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleController : MonoBehaviour
+{
+    public float velocity;
+    public float topBorder;
+    public float bottomBorder;
+
+    public string axis;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float move = Input.GetAxis(axis) * velocity * Time.deltaTime;
+        float nextPos = transform.position.y + move;
+        if (nextPos > topBorder)
+        {
+            move = 0;
+        }
+        if (nextPos < bottomBorder)
+        {
+            move = 0;
+        }
+        transform.Translate(0, move, 0);
+    }
+}
