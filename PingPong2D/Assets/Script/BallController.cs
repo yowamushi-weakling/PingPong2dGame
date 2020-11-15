2415cbe836f20d44c21b3406c11be166e6a06936
yowamushi-weakling <73294938+yowamushi-weakling@users.noreply.github.com>
2020-11-15 15:04:00 +0700

    Project Finish
    - Adding UI
    - Adding Sound

--- PingPong2D/Assets/Script/BallController.cs
+++ PingPong2D/Assets/Script/BallController.cs
@@ -1,18 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BallController : MonoBehaviour
 {
     public int force;
+    int scoreP1;
+    int scoreP2;
 
     Rigidbody2D rigid;
+
+    GameObject panelSelesai;
+
+    Text scoreUIP1;
+    Text scoreUIP2;
+    Text txPemenang;
+
+    AudioSource audio;
+    public AudioClip hitSound;
+    
     // Start is called before the first frame update
     void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
         Vector2 direction = new Vector2(2, 0).normalized;
         rigid.AddForce(direction * force);
+        scoreP1 = 0;
+        scoreP2 = 0;
+        scoreUIP1 = GameObject.Find("Score1").GetComponent<Text>();
+        scoreUIP2 = GameObject.Find("Score2").GetComponent<Text>();
+        panelSelesai = GameObject.Find("GameOverPanel");
+        panelSelesai.SetActive(false);
+        audio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -29,17 +49,38 @@ public class BallController : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        audio.PlayOneShot(hitSound);
         if(collision.gameObject.name == "TepiKanan")
         {
+            scoreP1 += 1;
+            ShowScore();
             ResetBall();
             Vector2 direction = new Vector2(-2, 0).normalized;
             rigid.AddForce(direction * force);
+            if(scoreP1 == 5)
+            {
+                panelSelesai.SetActive(true);
+                txPemenang = GameObject.Find("Winner").GetComponent<Text>();
+                txPemenang.text = "Player 1 Win";
+                Destroy(gameObject);
+                return;
+            }
         }
         if (collision.gameObject.name == "TepiKiri")
         {
+            scoreP2 += 1;
+            ShowScore();
             ResetBall();
             Vector2 direction = new Vector2(2, 0).normalized;
             rigid.AddForce(direction * force);
+            if (scoreP2 == 5)
+            {
+                panelSelesai.SetActive(true);
+                txPemenang = GameObject.Find("Winner").GetComponent<Text>();
+                txPemenang.text = "Player 2 Win";
+                Destroy(gameObject);
+                return;
+            }
         }
         if(collision.gameObject.name == "Paddle1" || collision.gameObject.name == "Paddle2")
         {
@@ -48,5 +89,12 @@ public class BallController : MonoBehaviour
             rigid.velocity = new Vector2(0, 0);
             rigid.AddForce(direction * force * 2);
         }
+    }
+
+    void ShowScore()
+    {
+        Debug.Log("Score P1: " + scoreP1 + " Score P2: " + scoreP2);
+        scoreUIP1.text = scoreP1 + "";
+        scoreUIP2.text = scoreP2 + "";
     }
 }
---
+++ PingPong2D/Assets/Script/MenuManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public bool isEscapeToExit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.Escape))
+        {
+            if (isEscapeToExit)
+            {
+                Application.Quit();
+            }
+            else
+            {
+                BackToMenu();
+            }
+        }
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
